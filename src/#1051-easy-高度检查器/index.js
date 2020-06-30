5af7bb6d682ad86255053707b596aa63a75f2c58
费陶勇 <2460392754@qq.com>
2020-06-30 16:53:27 +0800

    feat: leetcode-#1051

--- src/#1051-easy-高度检查器/index.js
+++ src/#1051-easy-高度检查器/index.js
@@ -1,13 +1,19 @@
 /*
  * @Author: Pocky
  * @Date: 2020-06-30 14:27:26
- * @LastEditTime: 2020-06-30 15:57:20
+ * @LastEditTime: 2020-06-30 16:53:10
  * @URL: https://leetcode-cn.com/problems/height-checker/
  */
 
+// ==========
+// 1.遍历查重
+// 时间复杂度：o(n^2)
+// 空间复杂度：o(1)
+// 执行用时：68 ms, 在所有 JavaScript 提交中击败了 87.65% 的用户
+// 内存消耗：34.9 MB, 在所有 JavaScript 提交中击败了 100.00% 的用户
+// ==========
+
 /**
- * 时间复杂度：o(n^2)
- * 空间复杂度：o(1)
  * @param {number[]} heights
  * @return {number}
  */
