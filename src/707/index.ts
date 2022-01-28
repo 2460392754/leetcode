aabc262d3e2530ac3193e4bb496b6d1c25a9eb03
费陶勇 <2460392754@qq.com>
2022-01-28 16:34:08 +0800

    707

--- src/707/index.ts
+++ src/707/index.ts
@@ -1,34 +1,110 @@
 /*
  * @Author: 费陶勇
- * @Date: 2022-01-12 09:21:25
- * @LastEditTime: 2022-01-12 09:21:53
+ * @Date: 2022-01-28 16:18:59
+ * @LastEditTime: 2022-01-28 16:32:55
  * @LastEditors: your name
- * @Description: https://leetcode-cn.com/problems/design-hashmap/
- * @FilePath: /leetcode/src/706/index.ts
+ * @Description: https://leetcode-cn.com/problems/design-linked-list/
+ * @FilePath: /leetcode/src/707/index.ts
  */
 
 () => {
     /**
-     * 执行用时： 188 ms , 在所有 TypeScript 提交中击败了 69.05% 的用户
-     * 内存消耗： 48.2 MB , 在所有 TypeScript 提交中击败了 64.29% 的用户
+     * 执行用时： 120 ms , 在所有 TypeScript 提交中击败了 95.35% 的用户
+     * 内存消耗： 49.7 MB , 在所有 TypeScript 提交中击败了 5.81% 的用户
      */
-    class MyHashMap {
-        private _hashMap: { [key: number]: number | undefined } = {};
+    class MyLinkedList {
+        private _head: ListNode = null;
+        private _size: number = 0;
 
-        constructor() {}
+        get(index: number): number {
+            // 处理边界
+            if (index > this._size - 1) {
+                return -1;
+            }
 
-        put(key: number, value: number): void {
-            this._hashMap[key] = value;
+            let tempHead = this._head;
+            let i = 0;
+
+            while (tempHead !== null) {
+                // 命中成功
+                if (i === index) {
+                    return tempHead.val;
+                }
+
+                tempHead = tempHead.next;
+                i++;
+            }
         }
 
-        get(key: number): number {
-            const val = this._hashMap[key];
-
-            return val === undefined ? -1 : val;
+        addAtHead(val: number): void {
+            this._head = new ListNode(val, this._head);
+            this._size++;
         }
 
-        remove(key: number): void {
-            this._hashMap[key] = undefined;
+        addAtTail(val: number, index: number | undefined): void {
+            if (this._head === null) {
+                this.addAtHead(val);
+                return;
+            }
+
+            let tempHead = this._head;
+            let i = 0;
+            this._size++;
+
+            while (tempHead !== null) {
+                // 添加尾部或对应的索引位置
+                if (tempHead.next === null || i === index - 1) {
+                    tempHead.next = new ListNode(val, tempHead.next);
+
+                    return;
+                }
+
+                tempHead = tempHead.next;
+                i++;
+            }
+        }
+
+        addAtIndex(index: number, val: number): void {
+            // 添加到头部
+            if (index <= 0) {
+                this.addAtHead(val);
+            }
+
+            // 处理对应索引
+            else if (index <= this._size) {
+                this.addAtTail(val, index);
+            }
+        }
+
+        deleteAtIndex(index: number): void {
+            // 删除不存在的索引
+            if (index < 0 || index > this._size) {
+                return;
+            }
+
+            // 删除链头
+            if (index === 0) {
+                this._size--;
+                this._head = this._head.next;
+                return;
+            }
+
+            let i = 0;
+            let tempHead = this._head;
+
+            while (tempHead !== null) {
+                // 命中
+                if (i + 1 === index) {
+                    // 非空节点
+                    if (tempHead.next !== null) {
+                        tempHead.next = tempHead.next.next;
+                        this._size--;
+                    }
+                }
+
+                tempHead = tempHead.next;
+                i++;
+            }
         }
     }
 };
